2718e2532d101366f7b994b3d24654090ef12227
timvisee <tim@visee.me>
2019-03-14 21:32:36 +0100

    Simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,36 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//! Renders a QR code to the console.
+//! Simple QR code renderer in the terminal.
+//!
+//! This is based on:
+//! - https://crates.io/crates/qair
+//! - https://code.willemp.be/willem/qair/src/branch/master/src/console_barcode_renderer.rs
 
 use std::sync::Arc;
 
 use crossterm::{style, Color, TerminalOutput};
 use qrcode::{
     QrCode,
-    types::Color::{Dark, Light},
+    types::Color::{self as QrColor, Dark as QrDark, Light as QrLight},
 };
+pub use qrcode::types::QrError;
+
+/// Quiet zone size in pixels around QR code.
+///
+/// Should be 4, but using 2 for small terminals:
+/// https://qrworld.wordpress.com/2011/08/09/the-quiet-zone/
+const QUIET_ZONE_WIDTH: usize = 2;
 
 /// Print the given `text` as QR code in the terminal.
-pub fn print_qr(text: &str) -> Result<(), String> {
+///
+/// Returns an error if generating the QR code failed.
+///
+/// # Panics
+///
+/// Panics if printing the QR code to the terminal failed.
+pub fn print_qr(text: &str) -> Result<(), QrError> {
     Renderer::new().print_qr(text)
 }
 
@@ -32,39 +49,43 @@ impl Renderer {
     }
 
     /// Print the given `text` as QR code in the terminal.
-    pub fn print_qr(&mut self, text: &str) -> Result<(), String> {
-        let code = QrCode::new(text).map_err(|err| format!("Cannot render QR code: {}", err))?;
-        let width = code.width();
-        let vec = code.into_colors();
+    ///
+    /// Returns an error if generating the QR code failed.
+    ///
+    /// # Panics
+    ///
+    /// Panics if printing the QR code to the terminal failed.
+    pub fn print_qr(&mut self, text: &str) -> Result<(), QrError> {
+        // Generate the code, obtain the QR code colors
+        let pixels = QrCode::new(text)?.into_colors();
 
-        // Should be 4, but 1 works fine and we don't have much space.
-        // (see https://qrworld.wordpress.com/2011/08/09/the-quiet-zone/)
-        let quiet_width = 1;
+        // Surround the code with quiet zone
+        let pixels = Self::surround_quiet(&pixels, QUIET_ZONE_WIDTH, QrLight);
 
-        let vec = Self::add_quiet_zone(&vec, width, quiet_width, Light);
-        self.print_barcode_vec(&vec, width + (quiet_width * 2));
+        // Print the code
+        self.print_matrix(&pixels);
         Ok(())
     }
 
-    /// Print a 2D barcode to the terminal.
+    /// Print a matrix describing a 2D barcode to the terminal.
     ///
     /// The barcode is given as 1D slice.
     ///
     /// # Panics
     ///
-    /// Panics if `vec_width` isn't `vec.len()` squared.
-    fn print_barcode_vec(&mut self, vec: &Vec<qrcode::types::Color>, vec_width: usize) {
-        assert_eq!(vec.len(), vec_width * vec_width);
+    /// Panics if the given matrix of `pixels` doens't have a length that is a multiple of 2.
+    fn print_matrix(&mut self, pixels: &[QrColor]) {
+        let width = usize_sqrt(pixels.len());
 
-        for row in 0..vec_width / 2 {
-            for col in 0..vec_width {
-                let vec_pos = (row * 2) * vec_width + col;
-                let vec_pos_below = (row * 2 + 1) * vec_width + col;
-                match (vec[vec_pos], vec[vec_pos_below]) {
-                    (Dark, Dark) => self.black_above_black(),
-                    (Dark, Light) => self.black_above_white(),
-                    (Light, Dark) => self.white_above_black(),
-                    (Light, Light) => self.white_above_white(),
+        for row in 0..width / 2 {
+            for col in 0..width {
+                let vec_pos = (row * 2) * width + col;
+                let vec_pos_below = (row * 2 + 1) * width + col;
+                match (pixels[vec_pos], pixels[vec_pos_below]) {
+                    (QrDark, QrDark) => self.black_above_black(),
+                    (QrDark, QrLight) => self.black_above_white(),
+                    (QrLight, QrDark) => self.white_above_black(),
+                    (QrLight, QrLight) => self.white_above_white(),
                 };
             }
             self.newline();
@@ -72,46 +93,46 @@ impl Renderer {
 
         // Because one character is two "pixels" above each other, the last pixel-line
         // has only white ("empty") "pixels" in case of an odd number of pixelrows.
-        if vec_width % 2 == 1 {
-            for col in 0..vec_width {
-                let vec_pos = vec_width * (vec_width - 1) + col;
-                match vec[vec_pos] {
-                    Dark => self.black_above_white(),
-                    Light => self.white_above_white(),
+        if width % 2 == 1 {
+            for col in 0..width {
+                let vec_pos = width * (width - 1) + col;
+                match pixels[vec_pos] {
+                    QrDark => self.black_above_white(),
+                    QrLight => self.white_above_white(),
                 };
             }
             self.newline()
         }
     }
 
-    /// Surround given 2D barcode with empty space.
+    /// Surround a given matrix with `quiet` pixels having the specified `thickness`.
     ///
-    /// The barcode is given as 1D slice.
+    /// The matrix is given as 1D slice.
     ///
     /// # Panics
     ///
-    /// Panics if `vec_width` isn't `vec.len()` squared.
-    fn add_quiet_zone<T: Copy>(
-        vec: &Vec<T>,
-        vec_width: usize,
-        quiet_width: usize,
+    /// Panics if the given matrix of `pixels` doens't have a length that is a multiple of 2.
+    fn surround_quiet<T: Copy>(
+        pixels: &[T],
+        thickness: usize,
         quiet: T,
     ) -> Vec<T> {
-        assert_eq!(vec.len(), vec_width * vec_width);
+        // Calculate widths
+        let width = usize_sqrt(pixels.len());
+        let out_width = width + thickness * 2;
 
-        let out_width = vec_width + quiet_width * 2;
-        let mut out = Vec::new();
-        out.resize(out_width * out_width, quiet);
-
-        for vec_row in 0..vec_width {
-            for vec_col in 0..vec_width {
-                let vec_pos = vec_width * vec_row + vec_col;
-                let out_row = vec_row + quiet_width;
-                let out_col = vec_col + quiet_width;
+        // Build the new pixel matrix, move given matrix in the center
+        let mut out = vec![quiet; out_width.pow(2)];
+        for vec_row in 0..width {
+            for vec_col in 0..width {
+                let vec_pos = width * vec_row + vec_col;
+                let out_row = vec_row + thickness;
+                let out_col = vec_col + thickness;
                 let out_pos = out_row * out_width + out_col;
-                out[out_pos] = vec[vec_pos]
+                out[out_pos] = pixels[vec_pos]
             }
         }
+
         out
     }
 
@@ -169,85 +190,58 @@ impl Renderer {
 
 #[cfg(test)]
 mod tests {
-    mod print_barcode_vec {
-        use super::super::*;
+    use super::*;
 
-        #[test]
-        #[should_panic]
-        fn incorrect_size() {
-            // Given: a barcode that is not square
-            let vec = vec![Dark, Dark, Light, Light, Light, Dark];
-
-            // When: trying rendering the vector on the terminal
-            let _ = Renderer::new().print_barcode_vec(&vec, 2);
-
-            // Then: panic occurred
-        }
+    /// Printing a matrix with the number of pixels not being a multiple of 2 fails.
+    #[test]
+    #[should_panic]
+    fn print_matrix_incorrect_size() {
+        Renderer::new().print_matrix(&vec![
+            QrDark,
+            QrDark,
+            QrLight,
+            QrLight,
+            QrLight,
+            QrDark,
+        ]);
     }
 
-    mod add_quiet_zone {
-        use super::super::*;
-
-        #[test]
-        fn empty() {
-            // Given: nothing
-
-            // When: adding quiet zone of 3 width to an empty vector
-            let actual_result = Renderer::add_quiet_zone::<i32>(&vec![], 0, 3, 7);
-
-            // Then: the result is a vector with quiet space above,left,under,right
-            let expected_result = vec![7; (3 * 2) * (3 * 2)];
-            assert_eq!(expected_result, actual_result);
-        }
-
-        #[test]
-        fn normal_case() {
-            // Given: a normal vector
-            let input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-            // When: adding quiet zone of 3 width to the given fector
-            let actual_result = Renderer::add_quiet_zone::<i32>(&input, 3, 3, 9);
-
-            // Then: the result is as expected
-            let expected_result = vec![
-                9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
-                9, 9, 0, 1, 2, 9, 9, 9, 9, 9, 9, 3, 4, 5, 9, 9, 9, 9, 9, 9, 6, 7, 8, 9, 9, 9, 9, 9,
-                9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
-            ];
-            assert_eq!(expected_result, actual_result);
-        }
-
-        #[test]
-        #[should_panic]
-        fn incorrect_size() {
-            // Given: a normal vector
-            let input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-            // When: the size is incorrect
-            let incorrect_size = 4;
-            let _ = Renderer::add_quiet_zone::<i32>(&input, incorrect_size, 3, 9);
-
-            // Then: panic occurred
-        }
+    #[test]
+    fn surround_quiet_normal() {
+        let input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let expected = vec![
+            9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+            9, 9, 0, 1, 2, 9, 9, 9, 9, 9, 9, 3, 4, 5, 9, 9, 9, 9, 9, 9, 6, 7, 8, 9, 9, 9, 9, 9,
+            9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+        ];
+        let actual = Renderer::surround_quiet(&input, 3, 9);
+        assert_eq!(expected, actual);
     }
 
-    mod print_qr {
-        use super::super::*;
-
-        #[test]
-        fn too_long() {
-            // Given: an input string exceeding the max length of data a QR code can contain
-            let character_a = 97;
-            let long_input = String::from_utf8(vec![character_a; 8000]).unwrap();
-
-            // When: printing a text to a QR code to the terminal
-            let actual_result = print_qr(&long_input);
-
-            // Then: there is an error
-            match actual_result {
-                Ok(_) => panic!(),
-                Err(_) => (),
-            }
-        }
+    #[test]
+    fn surround_quiet_empty() {
+        let actual = Renderer::surround_quiet(&[], 3, 7);
+        let expected = vec![7; (3 * 2) * (3 * 2)];
+        assert_eq!(expected, actual);
     }
+
+    /// Generating QR codes for text that is too large should fail.
+    #[test]
+    fn print_qr_too_long() {
+        print_qr(
+            &String::from_utf8(vec![b'a'; 8000]).unwrap(),
+        ).err().unwrap();
+    }
+}
+
+/// Take the square root of the given usize.
+///
+/// # Panics
+///
+/// Panics if the given number isn't a factor of 2.
+#[inline(always)]
+fn usize_sqrt(num: usize) -> usize {
+    let sqrt = (num as f64).sqrt() as usize;
+    assert_eq!(num, sqrt * sqrt, "given number isn't a multiple of 2");
+    sqrt as usize
 }
