226afd9e8895170bcbb3376d3d82c3e89d528457
Willem Penninckx <willem@willemp.be>
2020-05-01 18:02:59 +0200

    Add support for querying render size

--- src/util.rs
+++ src/util.rs
@@ -14,14 +14,15 @@ pub fn usize_sqrt(num: usize) -> usize {
 mod tests {
     use super::*;
 
-    /// Printing a matrix with the number of pixels not being a multiple of 2 fails.
     #[test]
     fn usize_sqrt_squared() {
+        assert_eq!(usize_sqrt(0), 0);
         assert_eq!(usize_sqrt(1), 1);
         assert_eq!(usize_sqrt(4), 2);
         assert_eq!(usize_sqrt(25), 5);
     }
 
+    /// Taking the integer square root of a number not being a multiple of 2 fails.
     #[test]
     #[should_panic]
     fn usize_sqrt_not_squared() {
