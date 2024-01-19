62c7b085b668ea05527a8bd9a2a43a7079fae5c9
Mihai Galos <mihai.galos@rbinternational.com>
2024-01-19 17:38:46 +0100

    feat: Examples - WiFi

---
+++ examples/example-wifi.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("WiFi network name: ");
+    let wifi_network = std::io::stdin().lines().next().unwrap().unwrap();
+    println!("\nPassword: ");
+    let pass = std::io::stdin().lines().next().unwrap().unwrap();
+    println!("\nType (WEP or [WPA - hit Enter as default]): ");
+    let _network_type = std::io::stdin().lines().next().unwrap().unwrap();
+    let network_type = if _network_type.is_empty() {
+        "WPA".to_string()
+    } else {
+        _network_type
+    };
+
+    let input = "WIFI:S:".to_string() + &wifi_network + ";T:" + &network_type + ";P:" + &pass + ";;";
+
+    qr2term::print_qr(input).unwrap();
+}
